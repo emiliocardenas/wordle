a993e0cc26201a2642a4b8d3e6ad026f9cf082a9
Emilio Cardenas <ecarden9@stedwards.edu>
2022-09-21 20:47:17 -0500

    change code structure to have words within react app

---
+++ src/wordLibrary/spanishWords.js
@@ -0,0 +1,24 @@
+export const spanishWordsImport = 
+[
+    "ANSIA", 
+    "ANION", 
+    "ANIMO", 
+    "MACRO", 
+    "GAFAS",
+    "SALEN",
+    "SECAN",
+    "SECAR",
+    "SEDAL",
+    "SEDAR",
+    "RENAL",
+    "NACER",
+    "NADEN",
+    "CESAR",
+    "CENAR",
+    "CELAR",
+    "CELAS",
+    "CENAS",
+    "CANES",
+    "CASES",
+
+]
